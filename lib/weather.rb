929ca5849bff7cc5fe6803fc1c720f4b3c8bcf12
jpcallanta <john@cnevids.com>
2015-03-01 23:13:00 -0500

    Adding console color and get/display forecast methods

--- lib/weather.rb
+++ lib/weather.rb
@@ -2,11 +2,13 @@
 
 require 'open-uri'
 require 'nokogiri'
+require 'colorize'
 
 class Weather
   def initialize(zipcode)
     @zipcode = zipcode
     @min_max_forecast = Hash.new
+    @forecast = Hash.new
     @xml_forecast = "http://graphical.weather.gov/xml/sample_products/browser_interface/ndfdBrowserClientByDay.php?zipCodeList=#{@zipcode}&format=12+hourly&numDays=10&Unit=e&snow=snow"
 
     begin
@@ -17,12 +19,19 @@ class Weather
     end
 
     @min_max_layout_key = get_min_max_layout_key
+    @forecast_key = get_forecast_key
     @time_line =
       @doc.xpath("//time-layout[layout-key='#{@min_max_layout_key}']").search('start-valid-time')
+    @time_line_forecast =
+      @doc.xpath("//time-layout[layout-key='#{@forecast_key}']").search('start-valid-time')
   end
 
   def get_min_max_layout_key
     return @doc.xpath('//time-layout/layout-key')[0].text
+  end
+
+  def get_forecast_key
+    return @doc.xpath('//time-layout/layout-key')[2].text
   end
 
   def get_min_max_temps
@@ -42,12 +51,41 @@ class Weather
     return @min_max_forecast
   end
 
+  def get_forecast
+    conditions =
+      @doc.xpath("//weather[@time-layout='#{@forecast_key}']").search('weather-conditions')
+    precip_pct =
+      @doc.xpath("//probability-of-precipitation[@time-layout='#{@forecast_key}']").search('value')
+    count = 0
+
+    @time_line_forecast.each do |t|
+      @forecast[t.attribute('period-name')] = {
+        condition: conditions[count].attribute('weather-summary'),
+        precip: precip_pct[count].text,
+      }
+
+      count += 1
+    end
+  end
+
+  def show_forecast
+    self.get_forecast
+
+    puts "Forecast, next #{(@forecast.count / 2).ceil} days for #{@zipcode}".colorize(:blue)
+
+    @forecast.each do |k, v|
+      if v[:condition].nil?
+        puts "#{k} - N/A"
+      else
+        puts "#{k} - #{v[:condition]} Chance of Precipitation:#{v[:precip]}%"
+      end
+    end
+  end
+
   def show_min_max_temps
     self.get_min_max_temps()
 
-    puts '-------------------------------------------------------'
-    puts "Temps, next #{@min_max_forecast.count} days for #{@zipcode}"
-    puts '-------------------------------------------------------'
+    puts "Temps, next #{@min_max_forecast.count} days for #{@zipcode}".colorize(:blue)
 
     @min_max_forecast.each do |k, v|
       if v[:min] == ''
